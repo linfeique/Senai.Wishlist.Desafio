4fcb01a4d191e6be73e4ad98896e75c2b294422d
Daniel Roncaglia Correia Dos Santos <28725446827@info.local>
2019-04-08 14:20:52 -0300

    comentarios no bd

--- ScriptsBD/SCRIPT01_DDL.sql
+++ ScriptsBD/SCRIPT01_DDL.sql
@@ -1,18 +1,24 @@
-/* Arquivo de criação de dados*/
+--Projeto de wishlist (lista de desejos)
+--Atividade do curso técnico Desenvolvimento de Sistemas
+--Senai Informática São Paulo - 2019
+--DDL - LINGUAGEM DE CRIAÇÃO DE DADOS
 
-
+--CRIA BANCO DE DADOS DO PROGRAMA
 create database senaiWishlistDesafio;
 
+--CRIA A TABELA DE USUARIOS
 create table USUARIOS (
+	-- COLUNA TIPO_DADOS CARACTERÍSTICAS CHAVE
 	ID int identity primary key
 	,EMAIL varchar(250) not null unique
 	,SENHA varchar(250) not null
 );
 
+--CRIA A TABELA DE DESEJOS
 create table DESEJOS (
+	-- COLUNA TIPO_DADOS CARACTERÍSTICAS CHAVE
 	ID int identity primary key
 	,DESEJO varchar(250) not null
 	,DATA_CRIACAO date not null
 	,ID_USUARIO int foreign key references USUARIOS(ID)
 );
-
