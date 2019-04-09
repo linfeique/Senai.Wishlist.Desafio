ac95517e9ca01ecbc497847700427255fb6f28ae
Daniel Roncaglia Correia Dos Santos <28725446827@info.local>
2019-04-09 14:40:40 -0300

    Arrumando o merge

--- Backend/Senai-Wishlist-API/Senai-Wishlist-API/Contexts/WishlistContext.cs
+++ Backend/Senai-Wishlist-API/Senai-Wishlist-API/Contexts/WishlistContext.cs
@@ -22,7 +22,7 @@ namespace Senai.Wishlist.API.Domains
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseSqlServer("Data Source=.\\SqlExpress; Initial Catalog= senaiWishlistDesafio; Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=.\\SqlExpress; Initial Catalog= SENAI_WISHLIST_DESAFIO; User Id=sa; Password=132");
             }
         }
 
